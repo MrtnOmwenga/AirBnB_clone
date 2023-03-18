672046daab09311c9292b6f2e51d60851f418daf
MrtnOmwenga <martin36449@gmail.com>
2023-03-18 12:08:08 -0700

    Debugging

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel():
 
     def __str__(self):
         """ Overrides __str__ """
-        return ("[{}] ({}) <{}>".format(type(self).__name__, self.id,
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id,
                                         self.__dict__))
 
     def save(self):
