a7015cf99b8a554d96340b8f0ae5cdd263f42e86
MrtnOmwenga <martin36449@gmail.com>
2023-03-18 12:56:49 -0700

    Debugging all() function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,7 +12,7 @@ class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns __objects"""
         if cls is not None:
             new_dict = {}
