72bcef77f3629f118935cefa26e3a7623684a164
MrtnOmwenga <martin36449@gmail.com>
2023-03-19 01:54:54 -0700

    Added missing files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,8 +32,19 @@ class FileStorage():
     def classes(self):
         """Return classess dictionary"""
         from models.base_model import BaseModel
-
-        classes = {"BaseModel": BaseModel}
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
 
     def reload(self):
