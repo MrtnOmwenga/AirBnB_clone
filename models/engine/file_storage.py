566081f1e228e43cad8075b043ef256024804e22
MrtnOmwenga <martin36449@gmail.com>
2023-03-20 00:09:10 -0700

    Debugging

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,8 +18,9 @@ class FileStorage():
 
     def new(self, obj):
         """Sets in __objects, the obj with key <obj class name>.id"""
-        key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to JSON file"""
